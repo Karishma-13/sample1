9c9f88181f2e541a144e60ca8dbff840f0078b10
Karishma-13 <karishma28111@gmail.com>
2024-01-04 00:25:39 +0530

    fixed error

--- gsapProj.js
+++ gsapProj.js
@@ -2,7 +2,7 @@
 //     ".imgDiv",
 //     {
 //         x: 500,
-//         duration : 5, //takes 5secs to show the animation 
+//         duration : 5, //takes 5secs to show the animation
 //         delay: 2,
 //         stagger: 4,  // it adds delay of 4secs in all 3 images used inside imgDiv class
 //         repeat: -1,
@@ -10,12 +10,15 @@
 // )
 
 
+
 // GSAP animation for moving images and revealing them one by one
 function createImageAnimation(index, xStart, xEnd, delay) {
+  // createImageAnimation() = used to animate each image in a sequence
   const currentImage = `.imgContainer img:nth-child(${index})`;
   const prevImage = `.imgContainer img:nth-child(${index - 1})`;
 
   gsap.fromTo(
+    // gsap.fromTo() = used to create the animation
     currentImage,
     { x: xStart },
     {
@@ -23,26 +26,28 @@ function createImageAnimation(index, xStart, xEnd, delay) {
       duration: 5,
       delay,
       x: xEnd,
-      ease: "power1.inOut",
       onStart: function () {
         gsap.to(prevImage, { opacity: 0, duration: 0 });
+
+        // onStart() callback = here when image reaches to "to" point or to the destination then we are making the visibility of prevImage as zero;
+        // onStart callback, is  hiding the previous image (prevImage) by setting its opacity to 0, whenm it reaches "to" point
       },
     }
   );
 }
 
-gsap.to(".imgContainer", {
-  x: "60%", // Move the container to the right
+gsap.to(".imgContainer", {      //gsap.to() = this animates the "imgContainer's  first image (first child)" 
+  x: "60%", // it moves the "imgContainer's first image (first child)" to the right
   duration: 5,
-  ease: "power1.inOut",
   onStart: function () {
-    // Show the next image
-    gsap.to(".imgContainer img", { opacity: 0, duration: 0 }); // Hide all images
-    gsap.to(".imgContainer img:first-child", { opacity: 1, duration: 0.5 }); // Show the first image
+    // onStart() callback = here it shows the next image
+    gsap.to(".imgContainer img", { opacity: 0, duration: 0 }); // hides all the images
+    gsap.to(".imgContainer img:first-child", { opacity: 1, duration: 0.5 }); // shows the current image
   },
   onComplete: function () {
-    // Reset position for the next cycle
+    // onComplete() callback = here it resets position of "imgContainer" for the next cycle & starts animation for other images
     gsap.to(".imgContainer", { x: "0%", duration: 0 });
+    gsap.to(".imgContainer img:first-child", { opacity: 0, duration: 0 }); // hides the first image as it was causing some issue
 
     createImageAnimation(2, "-100%", "90%", 0.5);
     createImageAnimation(3, "-200%", "-10%", 6);
@@ -50,13 +55,3 @@ gsap.to(".imgContainer", {
     createImageAnimation(5, "-400%", "-210%", 18);
   },
 });
-
-
-
-
-
-
-
-
-
-
